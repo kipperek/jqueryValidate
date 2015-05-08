0cd8e04592603fee969302d9d75a16046f549987
Kacper Czechowicz <kacper.czechowicz@traffit.com>
2015-05-08 21:47:23 +0200

    DONE

--- jqvalidate.js
+++ jqvalidate.js
@@ -3,12 +3,13 @@
 (function($){
 	'use strict';
 
-    var validateEmail = function(text){
-        var pattern = /^.+@.+[.].{2,}$/gi;
-        return pattern.test(text);
+    var validateEmail = function(text, input){
+        var pattern = /^.+@.+[.].{2,}$/gi,
+        valid = pattern.test(text);
+        setBorders(valid, input);
     };
 
-    var validatePassword = function(text, entropy){
+    var validatePassword = function(text, entropy, input){
         var passwordEntropy = 0,
         a = 35,
         b = 10,
@@ -23,25 +24,41 @@
         passwordEntropy = Math.log2(passwordEntropy);
         passwordEntropy *= length;
 
-        return passwordEntropy > entropy;
+        var valid = passwordEntropy > entropy;
+        setBorders(valid, input);
     };
 
-    var validatePostalCode = function(text){
+    var validatePostalCode = function(text, input, form){
         var pattern = /^[0-9]{2}-[0-9]{3}$/gi;
 
         if(pattern.test(text)){
+            $.ajax({
+                url: 'kody.json',
+                dataType: 'json'
+            }).done(function(json){
+                var kod = json[text],
+                miasto = typeof kod !== 'undefined' ? kod['miejscowość'] : '';
 
+                var $msg = input.next('span.city');
+                if($msg.length === 0){
+                    input.after('<span class="city">'+miasto+'</span>');
+                }else{
+                    $msg.text(miasto);
+                }
 
-
-            
+                var valid = miasto ? true : false;
+                setBorders(valid, input);
+                checkIfValid(form);
+            });
         }
 
         return false;
     };
 
-    var validateRegExp = function(text, regexp){
-        var pattern = new RegExp(regexp, 'gi');
-        return pattern.test(text);
+    var validateRegExp = function(text, regexp, input){
+        var pattern = new RegExp(regexp, 'gi'),
+        valid = pattern.test(text);
+        setBorders(valid, input);  
     };
 
     var setBorders = function(valid, input){
@@ -49,40 +66,44 @@
         input.removeClass('ok error').addClass(cl);
     };
 
-	var validateInput = function(input, settings){
+	var validateInput = function(input, settings, form){
 		var inputType = input.attr('data-validate'),
-        text = input.val(),
-        valid = false;
+        text = input.val();
+        setBorders(false, input); 
 
         switch(inputType){
-            case 'email': valid = validateEmail(text);
+            case 'email': validateEmail(text,input);
                 break;
-            case 'password': valid = validatePassword(text, settings.entropy);
+            case 'password': validatePassword(text, settings.entropy, input);
                 break;
-            case 'postalcode': valid = validatePostalCode(text);
+            case 'postalcode': validatePostalCode(text, input, form); 
                 break;
-            default: valid = validateRegExp(text, inputType);
+            default: validateRegExp(text, inputType, input);
         }
         
-        setBorders(valid, input); 
-
-        return valid;
 	};
 
-    var coreFunction = function(form, settings){
-        var valid = true,
-        $sendBtn = form.find('input[type=submit]');
-
-        form.find('input[data-validate]').each(function(i,el){
-            var $input = $(el);
-            valid = validateInput($input, settings) ? valid : false;
+    var checkIfValid = function(form){
+        var valid = true;
+        $.each(form.find('input[data-validate]'),function(i,el){
+            el = $(el);
+            valid = el.hasClass('error') ? false : valid;
         });
 
-        if(!valid && settings.block_sending){
-            $sendBtn.attr('disabled','disabled');
+        var $sendBtn = form.find('input[type=submit]');
+        if(!valid){
+            $sendBtn.attr('disabled', 'disabled');
         }else{
-             $sendBtn.removeAttr('disabled');
+            $sendBtn.removeAttr('disabled');
         }
+    };
+
+    var validateAllInputs = function(form, settings){
+        form.find('input[data-validate]').each(function(i,el){
+            var $input = $(el);
+            validateInput($input, settings, form);
+        });
+        checkIfValid(form);
     };
 
 	$.fn.jqValidate = function(options) {
@@ -95,14 +116,14 @@
         form = this;
 
         if(settings.validate_on_start){
-            coreFunction(form, settings);
+            validateAllInputs(form, settings);
         }
 
         this.find('input[data-validate]').on('input',function(){
-            coreFunction(form, settings);
+            validateInput($(this), settings, form);
+            checkIfValid(form);
         });
-	  	
-
+	  
         return this;
     };
 })(jQuery);
